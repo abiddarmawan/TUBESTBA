54b0316fdbb83c3b706db2f64ddc1e34c9fd932c
abid <abiddarmawan0202@gmail.com>
2022-06-14 14:13:30 +0700

    edit

--- TUBESTBA.py
+++ TUBESTBA.py
@@ -6,7 +6,7 @@ st.set_page_config(layout="wide")
 st.title("Lexical Analyzer")
 st.header("Grammar")
 
-col1, col2, col3 = st.columns(3)
+col1, col2, col3,clo4 = st.columns(4)
 
 
 with col1:
@@ -24,6 +24,8 @@ with col2:
     st.write("Bawak")
 with col3 : 
       st.image("https://img.freepik.com/free-vector/rafflesia-flower_9378-9.jpg?w=740")
+
+
 
     
 alpha = list(string.ascii_lowercase)
@@ -103,23 +105,23 @@ transition['q29', 'n'] = 'q21'
 idx = 0
 s = 'q0'
 curToken = ''
- 
-sentence = st.text_input("Masukkan Kata", "")
-sentence = sentence.lower()+'#'
-cek = st.button("cek hasil")
+with clo4 :
+  sentence = st.text_input("Masukkan Kata", "")
+  sentence = sentence.lower()+'#'
+  cek = st.button("cek hasil")
 
-while s != 'accept' :
-        now = sentence[idx]
-        curToken += now
-        s = transition[(s, now)]
-        if s == 'q5' :
-                st.write("curtoken :",curToken,"Valid")
-                curToken = ''
+  while s != 'accept' :
+          now = sentence[idx]
+          curToken += now
+          s = transition[(s, now)]
+          if s == 'q5' :
+                  st.write("curtoken :",curToken,"Valid")
+                  curToken = ''
 
-        if s == 'error' :
-                st.write("error")
-                break
-        idx+=1
+          if s == 'error' :
+                  st.write("error")
+                  break
+          idx+=1
 if s == 'accept' :
        st.success(f"Semua token di input  *'{sentence}'* Valid")
 else : 
