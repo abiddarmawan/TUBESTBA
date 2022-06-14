2f410f99aa52437d47c5a00d9ff920475ae25944
abid <abiddarmawan0202@gmail.com>
2022-06-14 13:58:27 +0700

    edit

--- TUBESTBA.py
+++ TUBESTBA.py
@@ -2,7 +2,7 @@ import streamlit as st
 import string 
 
 
-st.set_page_config(layout="wide")
+#st.set_page_config(layout="wide")
 
 st.image("https://img.freepik.com/free-vector/rafflesia-flower_9378-9.jpg?w=740")
 st.title("Lexical Analyzer dan parser")
