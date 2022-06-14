20cb25b0dc99a54d5006f5fb7db373ac99154761
abid <abiddarmawan0202@gmail.com>
2022-06-14 14:55:04 +0700

    edit

--- TUBESTBA.py
+++ TUBESTBA.py
@@ -5,7 +5,7 @@ import string
 #st.set_page_config(layout="wide")
 #st.image("https://img.freepik.com/free-vector/rafflesia-flower_9378-9.jpg?w=300")
 st.title("Lexical Analyzer dan parser")
-
+st.header("BAHASA BENGKULU")
 st.header("Grammar")
 
 col1, col2, col3 = st.columns(3)
